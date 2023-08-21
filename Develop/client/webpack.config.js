cf4f0d95ac35d5d1135d6034f84833cf8dc39595
bcoleman56 <brettcoleman369@gmail.com>
2023-08-21 15:11:18 -0400

    bug fix

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -67,6 +67,7 @@ module.exports = () => {
             options: {
               presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
           }
         }
       ],
